dd6b352c13e0c0239d1301d6861a053e4a256606
gsbang97 <qkdrldbs12@gmail.com>
2022-01-07 13:43:26 +0900

    1056

---
+++ 3056.py
@@ -0,0 +1,19 @@
+i = list(map(int,input().split()))
+print(i[0]-i[1])
+
+
+
+# def out(arr1, arr2, sum):
+#     b = True
+#     if()
+
+# import sys
+# i = int(sys.stdin.readline())
+# man = [False]*i
+# p = []
+# for x in range(i):
+#     p.append(list(map(int,sys.stdin.readline().split())))
+# for x in range(i):
+#     man[p[x].index(min(p[x]))] = True
+# for x in range(i):
+    
